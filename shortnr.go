e8bcf92816a581694b3d6bc0236933c20c3005c9
Laytan <laytan@gravilux.nl>
2020-07-09 10:19:40 +0200

    hello world api server

--- shortnr.go
+++ shortnr.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(`{"message": "Hello, world!"}`))
+}
 
 func main() {
-	fmt.Println("Hello, world.")
+	r := mux.NewRouter()
+	api := r.PathPrefix("/api/v1").Subrouter()
+	api.HandleFunc("", HelloHandler).Methods(http.MethodGet)
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
