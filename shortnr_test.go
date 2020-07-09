eabf5ca4888975b2a5469ff1e4a68ac6032cb650
Laytan <laytan@gravilux.nl>
2020-07-09 09:55:36 +0200

    go fmt

--- shortnr_test.go
+++ shortnr_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestOnePlusOne(t *testing.T) {
-	answer := 1 + 1;
-	if(answer != 2) {
-		t.Error("One plus one is 2");
+	answer := 1 + 1
+	if answer != 2 {
+		t.Error("One plus one is 2")
 	}
 }
