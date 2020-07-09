e117454601439023d8a6096e6893242e849fd759
Laytan <laytan@gravilux.nl>
2020-07-09 10:35:21 +0200

    add json header middleware

--- shortnr.go
+++ shortnr.go
@@ -8,13 +8,21 @@ import (
 )
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte(`{"message": "Hello, world!"}`))
+}
+
+// Add json response header to request
+func JsonMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
 }
 
 func main() {
 	r := mux.NewRouter()
 	api := r.PathPrefix("/api/v1").Subrouter()
+	api.Use(JsonMiddleware)
 	api.HandleFunc("", HelloHandler).Methods(http.MethodGet)
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
