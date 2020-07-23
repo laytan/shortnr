1d9751ff9d3f7349f90e1ec8949fd54a9700e06b
laytan <laytanlaats@hotmail.com>
2020-07-23 10:44:20 +0200

    add vuejs v3 app as client

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  setup() {
+    return {
+      msg: 'Hello v3',
+    };
+  },
+};
+</script>
