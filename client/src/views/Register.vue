041c91cf4bc9e54558594916868d937650cfee52
laytan <laytanlaats@hotmail.com>
2020-07-25 22:05:10 +0200

    add alerts to register, keeping width from expanding with long text

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="mt-5 d-flex justify-content-center">
-    <div>
+  <div class="container-fluid mt-5 d-flex justify-content-center">
+    <div :style="{ maxWidth: '500px' }">
       <h1 class="h2">Create a free account!</h1>
+      <Alert v-if="success.length" type="success">
+        {{ success }}
+      </Alert>
+      <Alert v-if="error.length" type="danger">
+        {{ error }}
+      </Alert>
       <form @submit.prevent="doRegister" class="mb-3">
         <Email @changed="email = $event" class="mb-3"/>
         <Password auto-complete="new-password" @changed="password = $event" class="mb-3"/>
@@ -11,7 +17,7 @@
         <p class="m-0">
           Already have an account?
         </p>
-        <router-link to="/" class="text-secondary">Log in</router-link>
+        <router-link to="/" class="link-secondary">Log in</router-link>
       </div>
     </div>
   </div>
@@ -23,6 +29,7 @@ import { reqP } from '../api';
 import Email from '../components/forms/Email.vue';
 import Password from '../components/forms/Password.vue';
 import LoadingButton from '../components/forms/LoadingButton.vue';
+import Alert from '../components/Alert.vue';
 
 export default {
   name: 'Register',
@@ -30,6 +37,7 @@ export default {
     Email,
     Password,
     LoadingButton,
+    Alert,
   },
   setup() {
     const email = ref('');
@@ -41,10 +49,26 @@ export default {
     const error = ref('');
 
     const doRegister = () => {
+      // Reset alerts
       success.value = '';
       error.value = '';
 
+      // Validate required fields
+      if (!email.value.length || !password.value.length) {
+        error.value = 'Please fill in all fields';
+        return;
+      }
+
+      // Validate password length
+      if (password.value.length < 6) {
+        error.value = 'Password must be at least 6 characters';
+        return;
+      }
+
+      // Start showing loading
       loading.value = true;
+
+      // Request signup and handle success and errors setting loading=false at the end
       reqP('signup', {
         email: email.value,
         password: password.value,
