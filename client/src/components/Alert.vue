976d7d51da35b98146bb6ad85de71abc685b35f8
laytan <laytanlaats@hotmail.com>
2020-07-25 22:03:37 +0200

    add Alert component

---
+++ client/src/components/Alert.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    :class="`alert alert-${type}`"
+    role="alert">
+    <p class="sr-only">{{ type }} alert</p>
+    <h4 v-if="heading.length">heading</h4>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: 'info',
+    },
+    heading: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
