f9ec830d71634b5c84bf08d1a2a456600e2f3282
laytan <laytanlaats@hotmail.com>
2020-07-29 17:00:09 +0200

    ratelimit less aggresively

--- server/pkg/ratelimit/ratelimit.go
+++ server/pkg/ratelimit/ratelimit.go
@@ -8,7 +8,7 @@ import (
 
 // Define some defaults
 const GeneralRateLimit float64 = 1
-const AuthorizationRateLimit float64 = 0.1
+const AuthorizationRateLimit float64 = 0.5
 
 // Middleware returns the middleware used to ratelimit perSecond requests per second
 func Middleware(perSecond float64) func(next http.Handler) http.Handler {
