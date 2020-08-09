179b8767756477637576ac67e0fd67b2a07bcff4
Laytan Laats <Laytanlaats@hotmail.com>
2020-08-09 22:16:54 +0200

    don't collapse navbar ever

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,39 +1,27 @@
 <template>
   <div class="view-nav-container vh-100">
-    <nav class="navbar navbar-expand-md navbar-dark bg-secondary">
+    <nav class="navbar navbar-dark bg-secondary">
       <div class="container-fluid">
         <router-link class="navbar-brand text-primary" to="/">Shortnr</router-link>
-        <button
-          class="navbar-toggler"
-          data-toggle="collapse"
-          data-target="#primaryNav"
-          aria-controls="primaryNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="primaryNav">
-          <ul class="navbar-nav ml-auto mb-2 mb-md-0">
-            <li class="nav-item" v-if="!isLoggedIn">
-              <router-link class="nav-link" to="/login">Log in</router-link>
-            </li>
-            <li class="nav-item" v-if="!isLoggedIn">
-              <router-link class="nav-link" to="/register">Register</router-link>
-            </li>
-            <li class="nav-item" v-if="isLoggedIn">
-              <router-link class="nav-link" to="/dashboard">Dashboard</router-link>
-            </li>
-            <li class="nav-item" v-if="isLoggedIn">
-              <button
-              class="nav-link bg-transparent border-0"
-              style="font-weight: 600;"
-              @click="logout">
-                Log out
-              </button>
-            </li>
-          </ul>
-        </div>
+        <ul class="navbar-nav ml-auto flex-row">
+          <li class="nav-item" v-if="!isLoggedIn">
+            <router-link class="nav-link" to="/login">Log in</router-link>
+          </li>
+          <li class="nav-item" v-if="!isLoggedIn">
+            <router-link class="nav-link" to="/register">Register</router-link>
+          </li>
+          <li class="nav-item" v-if="isLoggedIn">
+            <router-link class="nav-link" to="/dashboard">Dashboard</router-link>
+          </li>
+          <li class="nav-item" v-if="isLoggedIn">
+            <button
+            class="nav-link bg-transparent border-0"
+            style="font-weight: 600;"
+            @click="logout">
+              Log out
+            </button>
+          </li>
+        </ul>
       </div>
     </nav>
     <router-view class="view" />
@@ -82,5 +70,9 @@ export default {
   .view {
     flex-grow: 1;
   }
+}
+
+.navbar-nav li + li {
+  margin-left: 1rem;
 }
 </style>
